f5a346438ca65901b3668d0a0e74111581e2464d
Claudio Wiedenegger <cwiedenegger@student.tgm.ac.at>
2026-01-09 20:17:14 +0100

    Axios Interceptor für JWT Token; request and response Interceptor

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,35 @@
 import axios from 'axios'
+import AuthService from './AuthService'
 
 const api = axios.create({
-    baseURL: 'localhost:8081/api/',
+    baseURL: 'http://localhost:8081/api/',
     headers: {
         'Content-Type': 'application/json'
     }
 })
 
-export default api
+// Request Interceptor: Token anhängen
+api.interceptors.request.use(
+    config => {
+        const user = AuthService.getCurrentUser();
+        if (user && user.token) {
+            config.headers['Authorization'] = 'Bearer ' + user.token;
+        }
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+// Response Interceptor - NUR loggen, NICHT ausloggen
+api.interceptors.response.use(
+    response => response,
+    error => {
+        console.log('API Error:', error.response?.status, error.response?.data);
+        // KEIN logout, KEIN reload - einfach Fehler weitergeben
+        return Promise.reject(error);
+    }
+);
+
+export default api;
